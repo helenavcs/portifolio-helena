ce7abbc300b50bd02654e4bbae08c63ac3b2c4ec
Helena Vitória <helenavitoriacsilva9@hotmail.com>
2026-06-08 18:49:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function mostrarPagina(idPagina) {
   const paginaSelecionada = document.getElementById(idPagina);
   if (paginaSelecionada) {
     paginaSelecionada.classList.add('ativo');
-    // Salva a última página acessada apenas na sessão atual
+
     sessionStorage.setItem("ultimaPagina", idPagina);
   }
 }
 
-// Quando a página recarregar, restaura a última seção
+
 window.addEventListener("load", () => {
   const ultimaPagina = sessionStorage.getItem("ultimaPagina");
   if (ultimaPagina) {
     mostrarPagina(ultimaPagina);
   } else {
-    // Se não houver nada salvo (nova guia), abre a primeira página por padrão
+
     const primeiraPagina = document.querySelector(".pagina");
     if (primeiraPagina) {
       primeiraPagina.classList.add("ativo");
@@ -28,7 +28,7 @@ window.addEventListener("load", () => {
 
 document.addEventListener("contextmenu", function(e) {
   if (e.target.tagName === "IMG") {
-    e.preventDefault(); // bloqueia menu de contexto em imagens
+    e.preventDefault();
   }
 });
 
